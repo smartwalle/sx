dad267c7d8fd9aafc9f7d30eef91e7e437a75261
smartwalle <smartwalle@gmail.com>
2021-07-23 09:14:22 +0800

    test

--- triefilter_test.go
+++ triefilter_test.go
@@ -5,18 +5,45 @@ import (
 	"testing"
 )
 
-func BenchmarkTrieFilter_Contains(b *testing.B) {
-	var fs, _ = sx.NewFileStock("./ck.txt")
-	var filter = sx.NewTrieFilter(fs)
+func getFilter() sx.Filter {
+	var stock, _ = sx.NewMemoryStock("福音会", "中国教徒", "统一教", "观音法门", "清海无上师", "盘古", "李洪志", "志洪李", "李宏志", "轮功", "法轮", "轮法功", "三去车仑")
+	var filter = sx.NewTrieFilter(stock)
 	filter.Excludes('-')
-	b.Log(filter.Contains("sss福音会"))
+	return filter
+}
+
+func BenchmarkTrieFilter_Contains(b *testing.B) {
+	var filter = getFilter()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		filter.Contains("台“[中国教徒]”造谣大陆没报道郑州水灾，被连线观众[清海无上师]当场打脸")
+	}
 }
 
 func BenchmarkTrieFilter_FindFirst(b *testing.B) {
+	var filter = getFilter()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		filter.FindFirst("台“[中国教徒]”造谣大陆没报道郑州水灾，被连线观众[清海无上师]当场打脸")
+	}
 }
 
 func BenchmarkTrieFilter_FindAll(b *testing.B) {
+	var filter = getFilter()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		filter.FindAll("台“[中国教徒]”造谣大陆没报道郑州水灾，被连线观众[清海无上师]当场打脸")
+	}
 }
 
 func BenchmarkTrieFilter_ReplaceRune(b *testing.B) {
+	var filter = getFilter()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		filter.ReplaceRune("台“[中国教徒]”造谣大陆没报道郑州水灾，被连线观众[清海无上师]当场打脸", '*')
+	}
 }
