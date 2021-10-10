b67fa796c891e513a4575f3a390f5e5a4f810824
SmartWalle <smartwalle@gmail.com>
2021-10-10 10:22:39 +0800

    fmt

--- cmd/main.go
+++ cmd/main.go
@@ -12,10 +12,10 @@ func main() {
 	fmt.Println(filter.Contains("sss福音会"))
 	fmt.Println(filter.Contains("福音会"))
 	fmt.Println(filter.Contains("s刘广智空军sss福.音会"))
-	fmt.Println(filter.FindFirst("福福音会"))  // bug
-	fmt.Println(filter.FindFirst("福-音会"))  // bug
-	fmt.Println(filter.FindFirst("福音会ss")) // bug
-	fmt.Println(filter.FindFirst("s福音会"))  // bug
+	fmt.Println(filter.FindFirst("福福音会"))
+	fmt.Println(filter.FindFirst("福-音会"))
+	fmt.Println(filter.FindFirst("福音会ss"))
+	fmt.Println(filter.FindFirst("s福音会"))
 	fmt.Println(filter.FindAll("sss刘广智空军sss福音会"))
 	fmt.Println(filter.FindAll("sstrychnine福音会"))
 	fmt.Println(filter.Replace("s刘广智空军sss福音会", '*'))
